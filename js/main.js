eda4deec901a250200bf644a59d96c52d1ba6417
naters123 <65196090+naters123@users.noreply.github.com>
2021-09-25 15:15:59 -0600

    chapter 4 stuff
    chapter 4 reading

--- js/main.js
+++ js/main.js
@@ -36,8 +36,71 @@ function getIndex() {
 	}
 }
 
+// same from the reading
 function chapter2() {
 	const question = "What is Superman's real name?"
 	const answer = prompt(question);
 	alert(`You answered ${answer}`)
 }
+
+// slightly modified from the reading
+function chapter3() {
+
+	let score = 0;
+
+	quiz = [
+		["What is Superman's real name?","Clark Kent"],
+		["What is Wonder Woman's real name?","Diana Prince"],
+		["What is Batman's real name?","Bruce Wayne"],
+		["How many questions have you gotten correct so far?", score] // a dynamic question
+	];
+
+	for(const [question,answer] of quiz){
+		const response = prompt(question);
+
+		// This allows the answer to be non case sensitive
+		if(response.toLowerCase() === answer.toString().toLowerCase()){
+			alert('Correct!');
+			score++;
+			quiz[3][1]++;
+
+		} else {
+			alert(`Wrong! The correct answer was ${answer}`);
+		}
+	}
+	alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);
+}
+// same from the reading
+function chapter4() {
+	const quiz = [
+		["What is Superman's real name?","Clark Kent"],
+		["What is Wonder Woman's real name?","Diana Prince"],
+		["What is Batman's real name?","Bruce Wayne"]
+	];
+	function start(quiz){
+		let score = 0;
+		// main game loop
+		for(const [question,answer] of quiz){
+			const response = ask(question);
+			check(response,answer);
+		}
+		// end of main game loop
+		gameOver();
+		// function declarations
+		function ask(question){
+			return prompt(question);
+		}
+		function check(response,answer){
+			if(response === answer){
+			alert('Correct!');
+			score++;
+			} else {
+			alert(`Wrong! The correct answer was ${answer}`);
+			}
+		}
+		function gameOver(){
+			alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);
+		}
+	}
+	start(quiz);	
+} 
