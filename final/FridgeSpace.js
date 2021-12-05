636bb1b91474db378a5f1a54ab38fcb7f0d82975
naters123 <65196090+naters123@users.noreply.github.com>
2021-12-04 22:08:35 -0700

    update
    asdas das dasd

--- final/FridgeSpace.js
+++ final/FridgeSpace.js
@@ -191,16 +191,7 @@ function openForm() {
 function closeForm() {
 	document.getElementById("addFood").style.display = "none";
 }
-
-// Randomize array, was able to write this after seeing a stack overflow post about it
-// Src: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 function shuffleArray(array) {
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
 	return array;
 }
 
